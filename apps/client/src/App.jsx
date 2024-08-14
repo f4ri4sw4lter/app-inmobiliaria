724ad2a0744efe112763416602fbcd0790bdce27
f4ri4sw4lter <fariaswalter95@gmail.com>
2024-08-14 18:02:52 -0300

    Fix pantalla de carga

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -5,32 +5,38 @@ import { useScrollToTop } from './hooks/use-scroll-to-top';
 import Router from './routes/sections';
 import LoginPage from './pages/login';
 import Cookies from 'js-cookie';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 // ----------------------------------------------------------------------
 
 export default function App() {
-  
+
   const [isLogged, setIsLogged] = useState(false);
-  const [User, setUser] = useState();
-  
+  const [User, setUser] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
   useScrollToTop();
 
   useEffect(() => {
-    if(Cookies.get('User')){
+    if (Cookies.get('User')) {
       setIsLogged(true);
       setUser(JSON.parse(Cookies.get('User')));
     }
-    if(isLogged){
-      setUser(JSON.parse(Cookies.get('User')));
-    }
-  },[isLogged])
-  
+    setIsLoading(false);
+  }, [isLogged])
+
   return (
-      <ThemeProvider>
-        {isLogged 
-          ? <Router User={User}/> 
-          : <LoginPage setIsLogged={setIsLogged} setUser={setUser}/>
-        }
-      </ThemeProvider>
+    <ThemeProvider>
+      {isLoading
+        ? <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <CircularProgress color="primary"/>
+          </Box>
+
+        : isLogged
+          ? <Router User={User} />
+          : <LoginPage setIsLogged={setIsLogged} setUser={setUser} />
+      }
+    </ThemeProvider>
   );
 }
